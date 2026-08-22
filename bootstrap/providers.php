73fa750069e4f5205101b0b0baf6c32ddd9e56c4
yakupudul <yakupudul@gmail.com>
2026-08-22 14:03:45 +0300

    Register Search Console property grouping provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -6,10 +6,12 @@ use App\Providers\HorizonServiceProvider;
 use App\Providers\RecurringAutomationServiceProvider;
 use App\Providers\SalesServiceProvider;
 use App\Providers\SearchConsoleCentralServiceProvider;
+use App\Providers\SearchConsoleGroupingServiceProvider;
 
 return [
     AppServiceProvider::class,
     SearchConsoleCentralServiceProvider::class,
+    SearchConsoleGroupingServiceProvider::class,
     AppPanelProvider::class,
     HorizonServiceProvider::class,
     RecurringAutomationServiceProvider::class,
