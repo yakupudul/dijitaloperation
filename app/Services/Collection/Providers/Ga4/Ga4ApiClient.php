dffbf8e75402f98b7c3434cf901024cd664bdc91
yakupudul <yakupudul@gmail.com>
2026-08-22 03:02:42 +0300

    feat: add GA4 read-only configuration endpoints for central metadata

--- app/Services/Collection/Providers/Ga4/Ga4ApiClient.php
+++ app/Services/Collection/Providers/Ga4/Ga4ApiClient.php
@@ -13,51 +13,97 @@ use Illuminate\Http\Client\Response;
 final class Ga4ApiClient
 {
     public const string DATA_API_V1BETA = 'https://analyticsdata.googleapis.com/v1beta';
-
     public const string ADMIN_API_V1BETA = 'https://analyticsadmin.googleapis.com/v1beta';
 
     public function __construct(
         private readonly GoogleApiClient $http,
     ) {}
 
-    /**
-     * @param  array<string, mixed>  $body
-     */
+    /** @param array<string, mixed> $body */
     public function runReport(CoreIntegration $integration, string $propertyResourceName, array $body): Response
     {
-        $url = self::DATA_API_V1BETA.'/'.$propertyResourceName.':runReport';
-
-        return $this->http->post($integration, $url, $body, GoogleScopeRegistry::CAPABILITY_GA4);
+        return $this->http->post(
+            $integration,
+            self::DATA_API_V1BETA.'/'.$propertyResourceName.':runReport',
+            $body,
+            GoogleScopeRegistry::CAPABILITY_GA4,
+        );
     }
 
     public function getMetadata(CoreIntegration $integration, string $propertyResourceName): Response
     {
-        $url = self::DATA_API_V1BETA.'/'.$propertyResourceName.'/metadata';
-
-        return $this->http->get($integration, $url, [], GoogleScopeRegistry::CAPABILITY_GA4);
+        return $this->http->get(
+            $integration,
+            self::DATA_API_V1BETA.'/'.$propertyResourceName.'/metadata',
+            [],
+            GoogleScopeRegistry::CAPABILITY_GA4,
+        );
     }
 
-    /**
-     * @param  array<string, mixed>  $body
-     */
+    /** @param array<string, mixed> $body */
     public function checkCompatibility(CoreIntegration $integration, string $propertyResourceName, array $body): Response
     {
-        $url = self::DATA_API_V1BETA.'/'.$propertyResourceName.':checkCompatibility';
-
-        return $this->http->post($integration, $url, $body, GoogleScopeRegistry::CAPABILITY_GA4);
+        return $this->http->post(
+            $integration,
+            self::DATA_API_V1BETA.'/'.$propertyResourceName.':checkCompatibility',
+            $body,
+            GoogleScopeRegistry::CAPABILITY_GA4,
+        );
     }
 
     public function getProperty(CoreIntegration $integration, string $propertyResourceName): Response
     {
-        $url = self::ADMIN_API_V1BETA.'/'.$propertyResourceName;
-
-        return $this->http->get($integration, $url, [], GoogleScopeRegistry::CAPABILITY_GA4);
+        return $this->adminGet($integration, $propertyResourceName);
     }
 
-    public function listDataStreams(CoreIntegration $integration, string $propertyResourceName): Response
+    /** @param array<string, mixed> $query */
+    public function listDataStreams(CoreIntegration $integration, string $propertyResourceName, array $query = []): Response
     {
-        $url = self::ADMIN_API_V1BETA.'/'.$propertyResourceName.'/dataStreams';
+        return $this->adminGet($integration, $propertyResourceName.'/dataStreams', $query);
+    }
 
-        return $this->http->get($integration, $url, [], GoogleScopeRegistry::CAPABILITY_GA4);
+    /** @param array<string, mixed> $query */
+    public function listKeyEvents(CoreIntegration $integration, string $propertyResourceName, array $query = []): Response
+    {
+        return $this->adminGet($integration, $propertyResourceName.'/keyEvents', $query);
+    }
+
+    public function getDataRetentionSettings(CoreIntegration $integration, string $propertyResourceName): Response
+    {
+        return $this->adminGet($integration, $propertyResourceName.'/dataRetentionSettings');
+    }
+
+    public function getAttributionSettings(CoreIntegration $integration, string $propertyResourceName): Response
+    {
+        return $this->adminGet($integration, $propertyResourceName.'/attributionSettings');
+    }
+
+    /** @param array<string, mixed> $query */
+    public function listCustomDimensions(CoreIntegration $integration, string $propertyResourceName, array $query = []): Response
+    {
+        return $this->adminGet($integration, $propertyResourceName.'/customDimensions', $query);
+    }
+
+    /** @param array<string, mixed> $query */
+    public function listCustomMetrics(CoreIntegration $integration, string $propertyResourceName, array $query = []): Response
+    {
+        return $this->adminGet($integration, $propertyResourceName.'/customMetrics', $query);
+    }
+
+    /** @param array<string, mixed> $query */
+    public function listGoogleAdsLinks(CoreIntegration $integration, string $propertyResourceName, array $query = []): Response
+    {
+        return $this->adminGet($integration, $propertyResourceName.'/googleAdsLinks', $query);
+    }
+
+    /** @param array<string, mixed> $query */
+    private function adminGet(CoreIntegration $integration, string $resource, array $query = []): Response
+    {
+        return $this->http->get(
+            $integration,
+            self::ADMIN_API_V1BETA.'/'.$resource,
+            $query,
+            GoogleScopeRegistry::CAPABILITY_GA4,
+        );
     }
 }
