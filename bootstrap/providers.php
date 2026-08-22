ffbf6a37c916939ba24daec637949ae48d743efa
yakupudul <yakupudul@gmail.com>
2026-08-22 12:59:57 +0300

    Enable Search Console central service provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,9 +5,11 @@ use App\Providers\Filament\AppPanelProvider;
 use App\Providers\HorizonServiceProvider;
 use App\Providers\RecurringAutomationServiceProvider;
 use App\Providers\SalesServiceProvider;
+use App\Providers\SearchConsoleCentralServiceProvider;
 
 return [
     AppServiceProvider::class,
+    SearchConsoleCentralServiceProvider::class,
     AppPanelProvider::class,
     HorizonServiceProvider::class,
     RecurringAutomationServiceProvider::class,
