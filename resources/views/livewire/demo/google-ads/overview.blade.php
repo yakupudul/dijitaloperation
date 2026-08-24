88047723881697e195551f88c855a64fc546aaa7
yakupudul <yakupudul@gmail.com>
2026-08-24 14:42:43 +0300

    fix: route Search workspace through live provider fallback

--- resources/views/livewire/demo/google-ads/overview.blade.php
+++ resources/views/livewire/demo/google-ads/overview.blade.php
@@ -121,7 +121,7 @@
         @include('livewire.demo.google-ads.tabs.campaigns')
     @elseif ($tab === 'search_demand')
         @if ($searchExpertWorkspace ?? false)
-            @include('livewire.demo.google-ads.tabs.search-expert-v2')
+            @include('livewire.demo.google-ads.tabs.search-expert-live')
         @else
             @include('livewire.demo.google-ads.tabs.search-demand')
             @include('livewire.demo.google-ads.tabs.search-negatives')
