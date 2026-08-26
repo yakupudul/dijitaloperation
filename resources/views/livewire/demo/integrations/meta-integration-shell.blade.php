88e8704e1ab63d0346b6b0d83ab3f1441a2872c1
yakupudul <yakupudul@gmail.com>
2026-08-27 01:33:05 +0300

    Embed Meta live collection monitor

---
+++ resources/views/livewire/demo/integrations/meta-integration-shell.blade.php
@@ -0,0 +1,9 @@
+<div>
+    @include('livewire.demo.integrations.meta-integration')
+
+    @if ($tab === 'connectors')
+        <div class="mt-6" wire:key="meta-ads-provider-live-monitor">
+            @livewire(\App\Livewire\Demo\Integrations\MetaAdsCollectionMonitor::class, key('meta-ads-provider-live-monitor'))
+        </div>
+    @endif
+</div>
